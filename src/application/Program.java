2ee9962587ad68107dd358e7f9ff0300be825e57
SalathielVieira <salathielvieira@outlook.com>
2020-09-22 13:06:17 -0400

    Implementar a interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,17 +3,15 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
-import java.util.function.Predicate;
 
 import entities.Product;
+import util.PriceUpdate;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("Tv", 900.00));
@@ -21,15 +19,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.00));
 		
-		double min = 100.0;
+		list.forEach(new PriceUpdate());
 		
-		list.removeIf(p -> p.getPrice() >= min);
-		
-		for(Product p : list) {
-			System.out.println(p);
-		}
-		
-		sc.close();
+		list.forEach(System.out::println);
 	}
 
 }
